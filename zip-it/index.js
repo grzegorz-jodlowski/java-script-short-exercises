0e7d258fb60078b6bb5170015962c68998241eaa
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-26 16:25:53 +0200

    Add zip-it exercises

---
+++ zip-it/index.js
@@ -0,0 +1,55 @@
+/*
+* Opanuj JavaScript - Przeprogramowani.pl
+* I. Fundamenty języka JavaScript
+*
+* Ćwiczenie 3 - "Przeplatanie"
+*/
+
+/*
+* Cel zadania
+*------------
+* Zaimplementuj funkcję, która dla dwóch podanych parametrów wykona "przeplatanie"
+* - używając kolejnych cyfr parametrów połączy je w jeden string.
+*
+* Przykład:
+* zipIt(111, 222) // => '121212'
+* zipIt(123, 456) // => '142536'
+* zipIt(12, 5555) // => '152555'
+*/
+
+/*
+* Punkty dodatkowe
+*-----------------
+* Funkcja powinna weryfikować, czy przekazane parametry są typu number. Jeśli parametry nie
+* spełniają tego warunku, funkcja powinna rzucić wyjątek.
+*/
+
+function zipIt(first, second) {
+  if (typeof first !== 'number' && typeof second !== 'number') throw new Error('Parameters are not a numbers');
+
+  const firstArray = `${first}`.split('');
+  const secondArray = `${second}`.split('');
+  const longestArrayLength = firstArray.length >= secondArray.length ? firstArray.length : secondArray.length;
+  const result = [];
+
+  for (let i = 0; i < longestArrayLength; i++) {
+    result.push(firstArray[i]);
+    result.push(secondArray[i]);
+  }
+
+  return result.join('');
+}
+
+/* Weryfikacja */
+
+function verify(input, goal) {
+  if (input === goal) {
+    console.log('Gratulacje!');
+  } else {
+    console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
+  }
+}
+
+verify(zipIt(111, 222), '121212');
+verify(zipIt(123, 456), '142536');
+verify(zipIt(12, 5555), '152555');
