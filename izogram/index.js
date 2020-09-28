13cbac42eab0e69eec930e1d5164f188e9333755
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-09-28 16:38:46 +0200

    isogram

---
+++ izogram/index.js
@@ -0,0 +1,34 @@
+/*
+* Opanuj JavaScript - Przeprogramowani.pl
+* I. Fundamenty języka JavaScript
+*
+* Ćwiczenie 13 - "Izogram"
+*/
+
+/*
+* Cel zadania
+*------------
+* Zaimplementuj funkcję sprawdzającą czy podane słowo jest izogramem, czyli słowem w którym każda litera występuje tylko jeden raz.
+*
+*
+* Przykład:
+* isogram('Izogram'); // => true
+* isogram('Przeprogramowani'); // => false
+*
+*/
+
+const isogram = word => new Set(word.toLowerCase()).size === word.length
+
+/* Weryfikacja */
+
+function verify(input, goal) {
+  if (input === goal) {
+    console.log('Gratulacje!');
+  } else {
+    console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
+  }
+}
+
+verify(isogram('izogram'), true);
+verify(isogram('Przeprogramowani'), false);
+verify(isogram('SprawdzAm'), false);
