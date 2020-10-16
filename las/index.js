e76493f44f382a068591823a405a60e62ceeb600
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-10-16 18:15:47 +0200

    Las exercises

---
+++ las/index.js
@@ -0,0 +1,39 @@
+
+const images = {
+  treasure: "💰",
+  leafs: "🍃",
+  tree: "🌲"
+};
+
+const trees = document.querySelectorAll(".tree");
+const button = document.querySelector(".button");
+const indexOfRandomTree = Math.floor(Math.random() * trees.length);
+
+const { treasure, leafs, tree } = images;
+
+const findTreasure = event => {
+  if (event.target.classList.contains("treasure")) {
+    trees.forEach(tree => {
+      tree.textContent = leafs;
+      tree.removeEventListener("mouseover", findTreasure);
+      tree.removeEventListener("mouseleave", hideTreasure);
+    });
+    event.target.textContent = treasure;
+    button.classList.toggle("show");
+  } else {
+    event.target.textContent = leafs;
+  }
+};
+const hideTreasure = event => {
+  event.target.textContent = tree;
+};
+
+trees.forEach((tree, index) => {
+  if (index === indexOfRandomTree) {
+    tree.classList.add("treasure");
+  }
+  tree.addEventListener("mouseover", findTreasure);
+  tree.addEventListener("mouseleave", hideTreasure);
+});
+
+button.addEventListener("click", () => location.reload());
