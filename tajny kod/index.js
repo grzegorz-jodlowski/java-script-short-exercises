38a6354f010fdce68908d8f895c263fcfabde9eb
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-10-19 19:05:24 +0200

    Tajny kod

--- tajny kod/index.js
+++ tajny kod/index.js
@@ -1,1 +1,32 @@
-document.querySelector('.display').textContent = '1234';
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll(".keyboard__button");
+
+let password = "";
+
+const reset = () => {
+  password = "";
+  display.textContent = password;
+};
+const save = () => {
+  if (password.length !== 4) {
+    display.textContent = "Hasło powinno mieć 4 cyfry - wpisz hasło jeszcze raz";
+    password = "";
+  } else {
+    display.textContent = password;
+    password = "";
+  }
+};
+
+const handleButtonClick = event => {
+  if (event.target.dataset.value === "-99") {
+    reset();
+  } else if (event.target.dataset.value === "99") {
+    save();
+  } else {
+    password += event.target.dataset.value;
+  }
+};
+
+buttons.forEach(button => {
+  button.addEventListener("click", handleButtonClick);
+});
