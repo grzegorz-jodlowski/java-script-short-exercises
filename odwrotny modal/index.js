73599cd767387e03991c2a4c214391497c1e229f
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-11-03 12:17:43 +0100

    Odwrotny modal - alternative solution

--- odwrotny modal/index.js
+++ odwrotny modal/index.js
@@ -22,3 +22,29 @@ modal.addEventListener("click", closeModal);
 modalBox.addEventListener("click", modalClick);
 modalCloseItems.forEach(item => item.addEventListener("click", closeModal));
 agreeButton.addEventListener("click", openModal);
+
+// const agreeButton = document.querySelector(".agree-button");
+// const modalBody = document.querySelector('.main-modal');
+
+// agreeButton.addEventListener('click', openModal);
+// document.addEventListener('click', closeModal);
+
+// function openModal() {
+//   modalBody.classList.remove('invisible');
+// }
+
+// function closeModal({target: clickedElement}) {
+//   if(checkConditions(clickedElement)){
+//     modalBody.classList.add('invisible');
+//   }
+// }
+
+// function checkConditions(clickedElement) {
+//   const isAuthorizedToModalClose = clickedElement.classList.contains('modal-close');
+//   const isMainModalSurroundings = clickedElement.classList.contains('main-modal');
+//   const isModalExitButton = clickedElement.closest('.modal-close');
+
+//   const conditions = [isAuthorizedToModalClose, isMainModalSurroundings, isModalExitButton];
+
+//   return conditions.some(condition => condition);
+// }
