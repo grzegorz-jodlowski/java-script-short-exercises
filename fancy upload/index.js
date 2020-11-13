be13042bdf924c702399bb82346f74160284c225
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-11-13 14:13:09 +0100

    Fancy upload - final version

--- fancy upload/index.js
+++ fancy upload/index.js
@@ -1,77 +1,39 @@
-const previewButton = document.querySelector(".preview-button");
-const nextButton = document.querySelector(".next-button");
+const fileInput = document.querySelector(".file-input");
+const fileDescriptionBox = document.querySelector(".file-description");
+const fileNameSpan = document.querySelector(".file-name");
+const fileTypeSpan = document.querySelector(".file-type");
+const fileSizeSpan = document.querySelector(".file-size");
+const buttonsContainer = document.querySelector(".buttons-container");
+const fileAttachButton = document.querySelector(".file-attach");
+const fileRemoveButton = document.querySelector(".file-remove");
 
-const progressItems = document.querySelectorAll(".item");
+const pickFile = () => {
+  if (fileInput.files[0]) {
+    const { name: fullName, size } = fileInput.files[0];
 
-const disabledButtonOpacity = "opacity-50";
-const disabledButtonCursor = "cursor-not-allowed";
+    const name = fullName.slice(0, fullName.lastIndexOf("."));
+    const type = fullName.slice(fullName.lastIndexOf(".") + 1, fullName.length);
+    const shortSize = `${(size / (1024 * 1024)).toFixed(2)}MB`;
 
-const filledCircle = `
-  <div class="w-10 h-10 bg-green mx-auto rounded-full text-lg text-white flex items-center">
-		<span class="text-white text-center w-full"><i class="fa fa-check w-full fill-current white"></i></span>
-	</div>`;
-const filledDash = fill => `
-  <div class="w-full bg-grey-light rounded items-center align-middle align-center flex-1">
-    <div class="bg-green-light text-xs leading-none py-1 text-center text-grey-darkest rounded " style="width: ${fill}%"></div>
-  </div>`;
-const emptyCircle = step => `
-	<div class="w-10 h-10 bg-white border-2 border-grey-light mx-auto rounded-full text-lg text-white flex items-center">
-	<span class="text-grey-darker text-center w-full">${step}</span>
-  </div>`;
+    fileDescriptionBox.classList.remove("hidden");
+    buttonsContainer.classList.remove("hidden");
 
-const emptyDash = `
-  <div class="w-full bg-grey-light rounded items-center align-middle align-center flex-1">
-    <div class="bg-green-light text-xs leading-none py-1 text-center text-grey-darkest rounded " style="width: 0%"></div>
-  </div>`;
-
-let step = 1;
-
-const updateStep = step => {
-  previewButton.classList.remove(disabledButtonOpacity);
-  previewButton.classList.remove(disabledButtonCursor);
-  nextButton.classList.remove(disabledButtonOpacity);
-  nextButton.classList.remove(disabledButtonCursor);
-  previewButton.removeAttribute("disabled");
-  nextButton.removeAttribute("disabled");
-
-  if (step <= 1) {
-    previewButton.classList.add(disabledButtonOpacity);
-    previewButton.classList.add(disabledButtonCursor);
-    previewButton.setAttribute("disabled", true);
+    fileNameSpan.innerHTML = name;
+    fileTypeSpan.innerHTML = type;
+    fileSizeSpan.innerHTML = shortSize;
+  } else {
+    fileDescriptionBox.classList.add("hidden");
+    buttonsContainer.classList.add("hidden");
   }
-  if (step >= 4) {
-    nextButton.classList.add(disabledButtonOpacity);
-    nextButton.classList.add(disabledButtonCursor);
-    nextButton.setAttribute("disabled", true);
-  }
-
-  progressItems.forEach(item => {
-    let { id, type } = item.dataset;
-    id = parseInt(id);
-    item.innerHTML = type === "circle" ? emptyCircle(id) : emptyDash;
-
-    if (id <= step && type === "circle") {
-      item.innerHTML = filledCircle;
-      item.classList.add = "animation";
-    } else if (id < step && type === "dash") {
-      item.innerHTML = filledDash(100);
-    } else if (id === step && type === "dash") {
-      item.innerHTML = filledDash(50);
-    }
-  });
+};
+const removeFile = () => {
+  fileInput.value = "";
+  pickFile();
+};
+const attachFile = () => {
+  alert("wysłano plik");
 };
 
-const changeStep = event => {
-  const id = event.target.dataset.id;
-  if (id === "-") {
-    --step;
-  } else if (id === "+") {
-    ++step;
-  }
-  updateStep(step);
-};
-
-previewButton.addEventListener("click", changeStep);
-nextButton.addEventListener("click", changeStep);
-
-updateStep(step);
+fileInput.addEventListener("change", pickFile);
+fileAttachButton.addEventListener("click", attachFile);
+fileRemoveButton.addEventListener("click", removeFile);
